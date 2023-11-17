07445517acf990894ce7b15ae840c6488755605e
davebryan <dave.tamakloe@gmail.com>
2023-11-17 20:06:03 +0000

    100 first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+
+/**
+ * pre_main_message - Prints a message before the main function is executed.
+ */
+void pre_main_message(void)
+{
+    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
