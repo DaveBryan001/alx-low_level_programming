803f16e065177c3c1185c0ffbb59eecf6e5ac4d0
davebryan <dave.tamakloe@gmail.com>
2023-11-18 03:51:16 +0000

    hot fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,17 @@
 section .data
-    hello db 'Hello, Holberton', 0
+hello db 'Hello, Holberton', 0
 
 section .text
-    global main
-
+global main
+extern printf
 main:
-    ; Set up the stack frame
-    push rbp
-    mov rdi, hello  ; First argument: format string
-    call printf     ; Call printf
-    mov eax, 0      ; Return 0 from main
+; Set up the stack frame
+push rbp
+mov rdi, hello  ; First argument: format string
+call printf     ; Call printf
+mov eax, 0      ; Return 0 from main
 
-    ; Clean up and exit
-    pop rbp
-    ret
+; Clean up and exit
+pop rbp
+ret
 
