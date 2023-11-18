6f832f7a7af140f81489157034356b94de0aa17a
davebryan <dave.tamakloe@gmail.com>
2023-11-18 04:04:04 +0000

    hot fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * pre_main_msg - Prints a message before the main function is executed.
  */
 
-void __attribute__ ((constructor)) pre_main_msg_()
+void __attribute__ ((constructor)) pre_main_msg()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
