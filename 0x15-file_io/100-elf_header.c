de44fdf52df273272957b27634beec1456ed8c93
davebryan <dave.tamakloe@gmail.com>
2023-11-28 23:24:43 +0000

    sigh

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -3,15 +3,24 @@
 
 #define BUF_SIZE 64
 
+/**
+ * display_error - Display an error message and exit with status code 98.
+ * @message: The error message to display.
+ */
 void display_error(const char *message)
 {
 	fprintf(stderr, "%s\n", message);
 	exit(98);
 }
 
+/**
+ * display_elf_header - Display information from the ELF header.
+ * @elf_header: Pointer to the ELF header structure.
+ */
 void display_elf_header(const Elf64_Ehdr *elf_header)
 {
 	int i;
+
 	printf("  Magic:   ");
 	for (i = 0; i < EI_NIDENT; ++i)
 	{
@@ -19,26 +28,41 @@ void display_elf_header(const Elf64_Ehdr *elf_header)
 	}
 	printf("\n");
 
-	printf("  Class:                             %s\n", (elf_header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
-	printf("  Data:                              %s\n", (elf_header->e_ident[EI_DATA] == ELFDATA2LSB) ? "2's complement, little endian" : "2's complement, big endian");
-	printf("  Version:                           %d\n", elf_header->e_ident[EI_VERSION]);
-	printf("  OS/ABI:                            %d\n", elf_header->e_ident[EI_OSABI]);
-	printf("  ABI Version:                       %d\n", elf_header->e_ident[EI_ABIVERSION]);
+	printf("  Class:                             %s\n",
+			(elf_header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
+	printf("  Data:                              %s\n",
+			(elf_header->e_ident[EI_DATA] == ELFDATA2LSB)
+			? "2's complement, little endian" : "2's complement, big endian");
+	printf("  Version:                           %d\n",
+			elf_header->e_ident[EI_VERSION]);
+	printf("  OS/ABI:                            %d\n",
+			elf_header->e_ident[EI_OSABI]);
+	printf("  ABI Version:                       %d\n",
+			elf_header->e_ident[EI_ABIVERSION]);
 
-	printf("  Type:                              %s\n", (elf_header->e_type == ET_EXEC) ? "EXEC (Executable file)" :
+	printf("  Type:                              %s\n",
+			(elf_header->e_type == ET_EXEC) ? "EXEC (Executable file)" :
 			(elf_header->e_type == ET_REL) ? "REL (Relocatable file)" :
 			(elf_header->e_type == ET_DYN) ? "DYN (Shared object file)" :
 			(elf_header->e_type == ET_CORE) ? "CORE (Core file)" : "Unknown");
 
-	printf("  Entry point address:               0x%lx\n", (unsigned long)elf_header->e_entry);
+	printf("  Entry point address:               0x%lx\n",
+			(unsigned long)elf_header->e_entry);
 }
 
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of command-line argument strings.
+ * Return: 0 on success, 98 on error.
+ */
 int main(int argc, char *argv[])
 {
 
 	Elf64_Ehdr elf_header;
 	const char *filename;
 	int fd;
+
 	if (argc != 2)
 	{
 		display_error("Usage: elf_header elf_filename");
