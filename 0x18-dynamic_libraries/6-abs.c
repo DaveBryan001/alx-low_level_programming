aa482372d0fa86aeadb2bf06635d85800fe513d3
davebryan <dave.tamakloe@gmail.com>
2024-01-15 16:43:06 +0000

    dynamic libraries

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the function parameter
+ * Return: absolute value of n;
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+
+}
